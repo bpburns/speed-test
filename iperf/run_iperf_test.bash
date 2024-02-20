96021637fc0c9a3d2e30d2293c96b73706798a21
Brian Barefoot Burns <3639671+bpburns@users.noreply.github.com>
2024-02-20 10:41:46 -0700

    Add script for running iperf3 tests.

---
+++ iperf/run_iperf_test.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+OUTPUT_DIR=test_results
+
+if [ -z ${SERVER} ]; then echo "Must specify SERVER"; exit; fi
+
+echo Will connect to $SERVER
+
+mkdir -p $OUTPUT_DIR
+
+TS=$(date +%s)
+
+# TCP tests
+echo Running TCP send test
+iperf3 -c $SERVER -J > $OUTPUT_DIR/$TS\_tcp_send.json
+
+echo Running TCP receive test
+iperf3 -c $SERVER -J --reverse > $OUTPUT_DIR/$TS\_tcp_receive.json
+
+# UDP tests
+echo Running UDP send test
+iperf3 -c $SERVER -J -u > $OUTPUT_DIR/$TS\_udp_send.json
+
+echo Running UDP receive test
+iperf3 -c $SERVER -J -u --reverse > $OUTPUT_DIR/$TS\_udp_receive.json
